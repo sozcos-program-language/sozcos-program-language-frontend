7f20b64a6cb76edf5a72c8689f31cda52bb0207c
Elsen <soozcoos@gmail.com>
2024-08-02 00:48:54 +0800

    vue3: vue3 的 setup 语法糖

---
+++ vue3/1_introduction/6-setup-plugin/README.md
@@ -0,0 +1,5 @@
+# VUE3 入门: 响应式
+
+### [文档地址](https://cn.vuejs.org/guide/essentials/reactivity-fundamentals.html)
+
+### [视频-setup语法糖](https://www.bilibili.com/video/BV1Za4y1r7KE?p=10&spm_id_from=pageDriver&vd_source=b5c04f54b8a7ce0b4d5deef9989f7f9f)
--- vue3/1_introduction/vue-introduction/src/components/Person.vue
+++ vue3/1_introduction/vue-introduction/src/components/Person.vue
@@ -6,22 +6,41 @@
   </div>
 </template>
 
+<!--
+    通过在标签添加setup属性, 声明这个是vue3语法, 并且通过 name声明当前的组件名称
+    组件安装方式: npm i vite-plugin-vue-setup-extend -D
+    随后在vite.config.js中进行配置
 
-<script>
-export default {
-  name: "Person",
-  data() {
-    return {
-      name: '张三',
-      age: '18',
-      tel: 18888888888,
-    }
-  },
-  methods:{
-    showTel(){
-      alert(this.tel)
-    }
-  }
+-->
+<script setup name="abc">
+/*
+  在vue的框架中, 每个页面都是一个组件, 组件要组测到根组件中,
+  并且可以通过插件的方式, 能够使用属性的方式来告诉 vue 组件名
+ */
+
+// 原始写法
+// export default {
+//   data() {
+//     return {
+//       name: '张三',
+//       age: '18',
+//       tel: 18888888888,
+//     }
+//   },
+//   methods:{
+//     showTel(){
+//       alert(this.tel)
+//     }
+//   }
+// }
+
+// vue3 写法
+let name = '张思';
+let age = 23;
+let tel = 18899911223;
+
+function showTel() {
+  alert(tel)
 }
 </script>
 
