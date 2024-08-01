35e6bcb79c5795485a291bd0c24a67b3ef5abfd1
Elsen <soozcoos@gmail.com>
2024-08-02 00:55:35 +0800

    vue3: vue3 的 setup 语法糖

---
+++ vue3/1_introduction/vue-introduction/classe-files/6-component-name-plugin/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <h1>你好啊</h1>
+  </div>
+  <Person/>
+</template>
+
+<script>
+import Person from "@/components/Person.vue";
+
+export default {
+  name: 'APP',
+  components: {Person}, // 引入组件名
+}
+</script>
+
+
+<style>
+.app {
+  background-color: #1b1b32;
+  color: orangered;
+}
+</style>
