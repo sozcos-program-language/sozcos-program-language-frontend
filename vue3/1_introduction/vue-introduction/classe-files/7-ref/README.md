cea1735e6ff30f50b9b76a0cf0ec48d64f1cc99c
Elsen <soozcoos@gmail.com>
2024-08-02 22:16:28 +0800

    vue3: assign 拷贝响应对象

--- vue3/1_introduction/vue-introduction/classe-files/7-ref/README.md
+++ vue3/1_introduction/vue-introduction/classe-files/7-ref/README.md
@@ -1,3 +1,6 @@
 # VUE3 入门: 基本类型的响应式
 
 ### [视频-setup语法糖](https://www.bilibili.com/video/BV1Za4y1r7KE?p=11&vd_source=b5c04f54b8a7ce0b4d5deef9989f7f9f)
+
+
+ref既可以包装基本类型, 也可以包装对象类型, 对于对象类型, 他底层也是使用 reactive 来包装
---
+++ vue3/1_introduction/vue-introduction/classe-files/7-ref/car.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="person">
+    <h2>reactive: 对象类型的响应式</h2>
+    <hr>
+    <p>一辆{{ car.name }}, 只要{{ car.price }}万</p>
+
+    <hr>
+
+    <h2>游戏列表:</h2>
+    <ul>
+      <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+    </ul>
+    <button @click="changeName">修改名称</button>
+  </div>
+</template>
+
+<script setup name="Car">
+import {ref} from "vue";
+
+let car = ref({
+  name: '奔驰',
+  price: 100
+})
+
+let games = ref([
+  {id: 'aefwerasd1', name: '游戏1'},
+  {id: 'aefwerasd2', name: '游戏3'},
+  {id: 'aefwerasd3', name: '游戏4'}
+])
+
+function changeName() {
+  games.value[0].name = '猪八戒';
+}
+</script>
+
+<style scoped>
+body {
+  background-color: #1b1b32;
+  color: orangered;
+  font-size: 30px;
+}
+
+.stage {
+  margin: 10px;
+  border: 2px solid orangered;
+  padding: 10px;
+  display: grid;
+}
+
+.stage * {
+  margin: 10px;
+  font-size: 25px;
+  font-weight: 800;
+}
+
+a {
+  color: white;
+}
+</style>
