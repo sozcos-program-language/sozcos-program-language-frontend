7361aa95cd4c4f0f96e19db6755cd5d032a4b2d7
余鑫-813451 <poozs@qq.com>
2024-08-06 17:00:10 +0800

    vue3: 样式绑定

--- vue3/1_introduction/vue-introduction/classe-files/11-watch/case1.vue
+++ vue3/1_introduction/vue-introduction/classe-files/11-watch/case1.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>样式绑定</h1>
+  <h1>watch 数据监听</h1>
   <a href="https://www.bilibili.com/video/BV1Za4y1r7KE?p=17&vd_source=b5c04f54b8a7ce0b4d5deef9989f7f9f">教程地址</a>
 
   <hr>
@@ -7,23 +7,25 @@
     watch 只监听 ref /reactive /getter 以及 上述内容的数据
   </p>
 
-  <div :class="{active: isActive, 'text-danger': hasError }">我是样式</div>
-  <button @click="changeSum">点我启用样式</button>
+  <h2>场景1: 监听 ref 定义的 基本类型 数据</h2>
+  <div>当前求和: {{ sum }}</div>
+  <button @click="changeSum">点我sum+1</button>
 
   <h2></h2>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
-let isActive = ref(false);
-let hasError = ref(true);
+let sum = ref(0);
 
 function changeSum() {
-  isActive.value = !isActive.value
-  hasError.value = !hasError.value
+  sum.value += 1
 }
 
+watch(sum, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+})
 </script>
 
 <style>
@@ -33,10 +35,6 @@ body {
   margin: 30px auto;
   font-size: 25px;
   padding-left: 30px;
-}
-
-.active {
-  background-color: orangered;
 }
 
 </style>
