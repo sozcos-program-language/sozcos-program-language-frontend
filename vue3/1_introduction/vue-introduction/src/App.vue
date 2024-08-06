09077b46d097eba64e426091b5fb83196236878e
余鑫-813451 <poozs@qq.com>
2024-08-06 17:29:55 +0800

    vue3: 尝试编写后天管理系统样式

--- vue3/1_introduction/vue-introduction/src/App.vue
+++ vue3/1_introduction/vue-introduction/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <Person/>
+  <Menu/>
 </template>
 
 <script>
 import Person from "@/components/Person.vue";
+import Menu from "@/components/menu.vue";
 
 export default {
   name: 'APP',
-  components: {Person}, // 引入组件名
+  components: {Person, Menu}, // 引入组件名
 }
 </script>
 
---
+++ vue3/1_introduction/vue-introduction/src/components/menu.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="menu">
+  <div>我是菜单栏</div>
+</div>
+</template>
+
+<style scoped>
+.menu{
+  width: 20%;
+  background-color: #0aba4e;
+}
+</style>
