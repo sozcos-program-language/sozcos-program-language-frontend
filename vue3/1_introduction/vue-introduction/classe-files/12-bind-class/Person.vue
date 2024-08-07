16556d63593a1b718f9b308a6f24f928aa6ccbb7
Elsen <soozcoos@gmail.com>
2024-08-07 23:35:47 +0800

    vue3: watch 对象监听

--- vue3/1_introduction/vue-introduction/classe-files/12-bind-class/Person.vue
+++ vue3/1_introduction/vue-introduction/classe-files/12-bind-class/Person.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>watch 数据监听</h1>
+  <h1>样式绑定</h1>
   <a href="https://www.bilibili.com/video/BV1Za4y1r7KE?p=17&vd_source=b5c04f54b8a7ce0b4d5deef9989f7f9f">教程地址</a>
 
   <hr>
@@ -7,29 +7,23 @@
     watch 只监听 ref /reactive /getter 以及 上述内容的数据
   </p>
 
-  <h2>场景1: 监听 ref 定义的 基本类型 数据</h2>
-  <div>当前求和: {{ sum }}</div>
-  <button @click="changeSum">点我sum+1</button>
+  <div :class="{active: isActive, 'text-danger': hasError }">我是样式</div>
+  <button @click="changeSum">点我启用样式</button>
 
   <h2></h2>
 </template>
 
 <script setup>
-import {ref, watch} from "vue";
+import {ref} from "vue";
 
-let sum = ref(0);
+let isActive = ref(false);
+let hasError = ref(true);
 
 function changeSum() {
-  sum.value += 1
+  isActive.value = !isActive.value
+  hasError.value = !hasError.value
 }
 
-let stopWatch = watch(sum, (newValue, oldValue) => {
-  console.log(newValue, oldValue);
-  // 当满足某个条件的时候停止监听
-  if (newValue >= 10) {
-    stopWatch()
-  }
-});
 </script>
 
 <style>
@@ -39,6 +33,10 @@ body {
   margin: 30px auto;
   font-size: 25px;
   padding-left: 30px;
+}
+
+.active {
+  background-color: orangered;
 }
 
 </style>
--- vue3/1_introduction/vue-introduction/src/App.vue
+++ vue3/1_introduction/vue-introduction/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Menu/>
+  <Person/>
 </template>
 
 <script>
