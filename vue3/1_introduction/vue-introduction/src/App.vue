1568bd5960b6e11928335075306729f616f61d6a
余鑫-813451 <poozs@qq.com>
2024-08-09 17:29:27 +0800

    个人站内容规划

--- vue3/1_introduction/vue-introduction/src/App.vue
+++ vue3/1_introduction/vue-introduction/src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <Person/>
+  <Menu/>
+  <Content/>
 </template>
 
 <script>
 import Person from "@/components/Person.vue";
 import Menu from "@/components/menu.vue";
+import Content from "@/components/content.vue";
 
 export default {
   name: 'APP',
-  components: {Person, Menu}, // 引入组件名
+  components: {Content, Person, Menu}, // 引入组件名
 }
 </script>
 
